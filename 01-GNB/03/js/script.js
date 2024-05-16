8bf838c54dfaba6eacc5690e0bb5175056c32355
무당선생 <yssmi@naver.com>
2024-05-16 11:42:16 +0900

    헤더 고정 및 마우스 휠 이벤트 추가

---
+++ 01-GNB/03/js/script.js
@@ -0,0 +1,39 @@
+$(function () {
+  // 대상을 변수에 저장
+  const $window = $(window);
+  const $header = $('header');
+  const $menu = $('.gnb > li');
+  const $submenu = $('.submenu');
+  const duration = 300;
+
+  // 메뉴 영역에 마우스가 들어오면
+  $menu.on('mouseenter', function () {
+    $submenu.stop().slideDown(duration);
+
+    // 활성화된 메뉴 표시 : on클래스 부여
+    $(this).addClass('on');
+
+    // header에 active 클래스 부여
+    $header.addClass('active');
+  });
+
+  // 메뉴 영역에서 마우스가 나가면
+  $menu.on('mouseleave', function () {
+    $submenu.stop().slideUp(duration);
+    $menu.removeClass('on');
+    $header.removeClass('active');
+  });
+
+  // 마우스 휠을 조작할 때
+  $window.on('wheel', function (e) {
+    // console.log(e);
+
+    if (e.originalEvent.wheelDelta > 0) {
+      // 휠을 올렸을 때
+      $header.removeClass('hide');
+    } else {
+      // 휠을 내렸을 때
+      $header.addClass('hide');
+    }
+  });
+});
