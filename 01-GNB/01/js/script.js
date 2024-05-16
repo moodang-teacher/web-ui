38a171e53323120a2db56daeee888372886ccda2
무당선생 <yssmi@naver.com>
2024-05-16 10:46:46 +0900

    최종완성

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -1,4 +1,26 @@
 // DOM 구조가 파악되면 실행
 $(function () {
-  alert('준비완료!');
+  // alert('준비완료!');
+
+  // 대상을 변수에 저장
+  const $menu = $('.gnb > li');
+  const $submenu = $('.submenu');
+  const duration = 300;
+
+  // 메뉴 영역에 마우스가 들어오면
+  $menu.on('mouseenter', function () {
+    // 그 영역 하위의 $submenu를 slideDown()
+    // $submenu.slideDown();
+    // stop() : 현재 진행 중인 애니메이션을 즉시 중지
+    $(this).find($submenu).stop().slideDown(duration);
+
+    // 활성화된 메뉴 표시 : on클래스 부여
+    $(this).addClass('on');
+  });
+
+  // 메뉴 영역에서 마우스가 나가면
+  $menu.on('mouseleave', function () {
+    $submenu.stop().slideUp(duration);
+    $menu.removeClass('on');
+  });
 });
