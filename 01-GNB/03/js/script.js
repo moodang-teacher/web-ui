c3e93709083a1e876c12f4be3c34308c50822db1
무당선생 <yssmi@naver.com>
2024-05-17 10:05:56 +0900

    기본 구조 및 디자인 완성

--- 01-GNB/03/js/script.js
+++ 01-GNB/03/js/script.js
@@ -5,15 +5,12 @@ $(function () {
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu');
   const duration = 300;
+  let lastScrollTop = 0;
 
   // 메뉴 영역에 마우스가 들어오면
   $menu.on('mouseenter', function () {
     $submenu.stop().slideDown(duration);
-
-    // 활성화된 메뉴 표시 : on클래스 부여
     $(this).addClass('on');
-
-    // header에 active 클래스 부여
     $header.addClass('active');
   });
 
@@ -24,16 +21,38 @@ $(function () {
     $header.removeClass('active');
   });
 
-  // 마우스 휠을 조작할 때
-  $window.on('wheel', function (e) {
-    // console.log(e);
-
-    if (e.originalEvent.wheelDelta > 0) {
-      // 휠을 올렸을 때
+  // 스크롤 방향에 따라 header의 hide 클래스를 토글하는 함수
+  function toggleHeaderVisibility(directionUp) {
+    if (directionUp) {
       $header.removeClass('hide');
     } else {
-      // 휠을 내렸을 때
       $header.addClass('hide');
     }
+  }
+
+  // throttle 함수 구현
+  function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+      const args = arguments;
+      const context = this;
+      if (!inThrottle) {
+        func.apply(context, args);
+        inThrottle = true;
+        setTimeout(() => inThrottle = false, limit);
+      }
+    };
+  }
+
+  // 마우스 휠을 조작할 때
+  $window.on('wheel', function (e) {
+    toggleHeaderVisibility(e.originalEvent.deltaY < 0);
   });
+
+  // 스크롤할 때 throttle 적용
+  $window.on('scroll', throttle(function () {
+    const scrollTop = $(this).scrollTop();
+    toggleHeaderVisibility(scrollTop < lastScrollTop);
+    lastScrollTop = scrollTop;
+  }, 250)); // 250ms 간격으로 throttle 적용
 });
