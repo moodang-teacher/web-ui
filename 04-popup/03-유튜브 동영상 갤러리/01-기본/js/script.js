d533c75d08fe5c9d8deba7da70212487c551b547
무당선생 <yssmi@naver.com>
2024-05-22 10:50:56 +0900

    동영상 자동 재생

--- 04-popup/03-유튜브 동영상 갤러리/01-기본/js/script.js
+++ 04-popup/03-유튜브 동영상 갤러리/01-기본/js/script.js
@@ -9,8 +9,11 @@ $(function () {
   // 비디오 리스트를 클릭(선택)했을 때
   $selectVideo.on('click', function () {
     // 선택한 비디오 링크를 받아서 변수에 저장
-    const videoLink = $(this).data('link'); /* data-link */
+    let videoLink = $(this).data('link'); /* data-link */
     console.log(videoLink);
+
+    // videoLink = videoLink + '?autoplay=1'
+    videoLink += '?autoplay=1';
 
     // $video의 src 값으로 비디오 링크를 세팅
     $video.attr('src', videoLink);
@@ -35,5 +38,8 @@ $(function () {
 
     // $videoWrap을 안 보이게
     $videoWrap.removeClass('active');
+
+    // $video의 src 값을 없애자 --> 동영상 삭제
+    $video.attr('src', '');
   });
 });
